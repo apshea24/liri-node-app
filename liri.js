ab937e23a4e7ce2d08914c508b2c804d3bc0bcec
shaggsworth <austin.shea89@gmail.com>
2018-10-25 22:36:32 -0700

    10_25_upload_2

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 var request = require("request");
 
+var moment = require("moment");
+
 var spotifyRequest = require("./keys.js")
 
 // var spotify = new Spotify(keys.spotify);
@@ -11,30 +13,32 @@ var serviceName = process.argv[2];
 
 switch (serviceName) {
     case "concert-this":
-    console.log("concert-this")
-    var artist = process.argv[3];
-    var bandsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-    // var eventsArr = "";
-    request(bandsURL, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            for (var property1 in body) {
-                    // eventsArr += body[property1]
-                    // console.log(eventsArr);
-                
+        console.log("concert-this")
+        var artist = process.argv[3];
+        var bandsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        // var eventsArr = "";
+        request(bandsURL, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(JSON.parse(body));
+                var eventArr = JSON.parse(body)
+                for (var i = 0; i < eventArr.length; i++) {
+                    console.log("Venue: " + eventArr[i].venue.name);
+                    console.log("Location: " + eventArr[i].venue.city + ", " + eventArr[i].venue.region);
+                    console.log("Datetime: " + moment("'" + eventArr[i].datetime + "'", moment.HTML5_FMT.DATETIME_LOCAL).format("L"));
+                }
+
             }
-            
-        }
-    });
-    break;
+        });
+        break;
     case "spotify-this-song":
-    console.log("spotify-this-song")
-    break;
+        console.log("spotify-this-song")
+        break;
     case "movie-this":
-    console.log("movie-this")
-    break;
+        console.log("movie-this")
+        break;
     case "do-what-it-says":
-    console.log("do-what-it-says")
-    break;
+        console.log("do-what-it-says")
+        break;
 }
 
 // var nodeArgs = process.argv;
