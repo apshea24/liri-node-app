daa8cf1969d7165dbaf73a867ff2f0dcda091e3c
shaggsworth <austin.shea89@gmail.com>
2018-10-25 22:56:05 -0700

    10_25_upload_3

--- liri.js
+++ liri.js
@@ -25,17 +25,39 @@ switch (serviceName) {
                     console.log("Venue: " + eventArr[i].venue.name);
                     console.log("Location: " + eventArr[i].venue.city + ", " + eventArr[i].venue.region);
                     console.log("Datetime: " + moment("'" + eventArr[i].datetime + "'", moment.HTML5_FMT.DATETIME_LOCAL).format("L"));
-                }
-
-            }
+                    console.log("--------------------------------------------------------------")
+                };
+            };
         });
         break;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
     case "spotify-this-song":
         console.log("spotify-this-song")
         break;
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
     case "movie-this":
-        console.log("movie-this")
+        var movie = process.argv[3];
+        var movieURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        request(movieURL, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                // console.log(JSON.parse(body));
+                var movieObj = JSON.parse(body);
+                console.log("Title: " + movieObj.Title);
+                console.log("Year Released: " + movieObj.Year);
+                console.log("IMDB Rating: " + movieObj.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + movieObj.Ratings[1].Value);
+                console.log("Produced In: " + movieObj.Country);
+                console.log("Language: " + movieObj.Language);
+                console.log("Plot: " + movieObj.Plot);
+                console.log("Actors/Actresses: " + movieObj.Actors);
+            };
+        });
+        console.log(movie);
         break;
+
+    //////////////////////////////////////////////////////////////////////////
     case "do-what-it-says":
         console.log("do-what-it-says")
         break;
